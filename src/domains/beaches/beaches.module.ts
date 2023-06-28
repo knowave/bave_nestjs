693ab345103d0a99db7369f662aa3bf3a9379448
knowave <kwaktaemin98@gmail.com>
2023-06-28 12:54:51 +0900

    import and provides

--- src/domains/beaches/beaches.module.ts
+++ src/domains/beaches/beaches.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BeachesService } from './beaches.service';
 import { BeachesController } from './beaches.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Beaches } from './entities/beaches.entity';
+import { BeachesRepository } from './beaches.repository';
 
 @Module({
-  providers: [BeachesService],
-  controllers: [BeachesController]
+  imports: [TypeOrmModule.forFeature([Beaches])],
+  providers: [BeachesService, BeachesRepository],
+  controllers: [BeachesController],
 })
 export class BeachesModule {}
