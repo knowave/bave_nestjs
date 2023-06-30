791d05c255d4e7cd73161b4cb237ae6605caac31
knowave <kwaktaemin98@gmail.com>
2023-06-30 14:03:22 +0900

    beaches service logic

--- src/domains/beaches/beaches.service.ts
+++ src/domains/beaches/beaches.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BeachesRepository } from './beaches.repository';
+import { MyPaginationQuery } from 'src/bases/pagination-query';
+import { Pagination, paginate } from 'nestjs-typeorm-paginate';
+import { Beaches } from './entities/beaches.entity';
+import { BEACH_EXCEPTION } from 'src/exception/error-code';
+import { promises } from 'dns';
 
 @Injectable()
-export class BeachesService {}
+export class BeachesService {
+  constructor(
+    @InjectRepository(BeachesRepository)
+    private readonly beachesRepository: BeachesRepository,
+  ) {}
+
+  async getAllBeaches(
+    options: MyPaginationQuery,
+  ): Promise<Pagination<Beaches>> {
+    const query = this.beachesRepository.createQueryBuilder('beach');
+    query.getRawMany();
+
+    const pagination = await paginate<Beaches>(query, options);
+
+    if (pagination.items.length === 0) {
+      throw new NotFoundException(BEACH_EXCEPTION.BEACH_NOT_FOUND);
+    }
+
+    return pagination;
+  }
+
+  async getBeachById(beachId: number): Promise<Beaches> {
+    return await this.beachesRepository.getBeachById(beachId);
+  }
+}
