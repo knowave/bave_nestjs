96db082cfa04b176f3b2914cef927d10883ec575
knowave <kwaktaemin98@gmail.com>
2023-06-30 14:02:16 +0900

    getBeachById

--- src/domains/beaches/beaches.repository.ts
+++ src/domains/beaches/beaches.repository.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Beaches } from './entities/beaches.entity';
 import { DataSource, Repository } from 'typeorm';
+import { BEACH_EXCEPTION } from 'src/exception/error-code';
 
 @Injectable()
 export class BeachesRepository extends Repository<Beaches> {
   constructor(private readonly dataSource: DataSource) {
     super(Beaches, dataSource.createEntityManager());
+  }
+
+  async getBeachById(beachId: number): Promise<Beaches> {
+    const beach = await this.findOne({ where: { beachId } });
+
+    if (!beach) {
+      throw new NotFoundException(BEACH_EXCEPTION.BEACH_NOT_FOUND);
+    }
+
+    return beach;
   }
 }
