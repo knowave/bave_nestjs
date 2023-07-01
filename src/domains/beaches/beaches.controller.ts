0bef65392f708955d19617e2e8056c113129b09f
knowave <kwaktaemin98@gmail.com>
2023-07-01 13:49:52 +0900

    beaches.controller

--- src/domains/beaches/beaches.controller.ts
+++ src/domains/beaches/beaches.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { BeachesService } from './beaches.service';
+import { MyPaginationQuery } from 'src/bases/pagination-query';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { Beaches } from './entities/beaches.entity';
 
 @Controller('beaches')
-export class BeachesController {}
+export class BeachesController {
+  constructor(private BeachesService: BeachesService) {}
+
+  @Get()
+  async getAllByBeaches(
+    @Query() options: MyPaginationQuery,
+  ): Promise<Pagination<Beaches>> {
+    return await this.BeachesService.getAllByBeaches(options);
+  }
+
+  @Get(':beachId')
+  async getBeachById(@Param('beachId') beachId: number): Promise<Beaches> {
+    return await this.BeachesService.getBeachById(beachId);
+  }
+}
