637d9f7408939250555c9015364a986aa7938e69
knowave <kwaktaemin98@gmail.com>
2023-07-02 14:27:19 +0900

    add User Exception, auth Exception

--- src/exception/error-code.ts
+++ src/exception/error-code.ts
@@ -4,3 +4,25 @@ export const BEACH_EXCEPTION = {
     message: '존재하는 해수욕장이 없습니다.',
   },
 };
+
+export const USER_EXCEPTION = {
+  USER_NOT_FOUND: {
+    code: 'USER_NOT_FOUND',
+    message: '존재하는 유저가 없습니다.',
+  },
+};
+
+export const AUTH_EXCEPTION = {
+  AUTH_CODE_EXPIRED: {
+    code: 'AUTH_CODE_EXPIRED',
+    message: 'Expired authentication code',
+  },
+  AUTH_CODE_INVALID: {
+    code: 'AUTH_CODE_INVALID',
+    message: 'Invalid authentication code',
+  },
+  AUTH_FAIL_VALIDATE: {
+    code: 'AUTH_FAIL_VALIDATE',
+    message: '아이디/비밀번호가 일치하지 않습니다',
+  },
+};
