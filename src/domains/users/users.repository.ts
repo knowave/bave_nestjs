2d888084aeaf5acedf9b5a26e8b77fc21b87e842
knowave <kwaktaemin98@gmail.com>
2023-07-02 14:26:48 +0900

    getUserById

--- src/domains/users/users.repository.ts
+++ src/domains/users/users.repository.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { Users } from './entities/users.entity';
+import { USER_EXCEPTION } from '../../exception/error-code';
 
 @Injectable()
 export class UsersRepository extends Repository<Users> {
   constructor(private readonly dataSource: DataSource) {
     super(Users, dataSource.createEntityManager());
+  }
+
+  async getUserById(userId: number): Promise<Users> {
+    const user = await this.findOne({ where: { userId } });
+
+    if (!user) {
+      throw new NotFoundException(USER_EXCEPTION.USER_NOT_FOUND);
+    }
+
+    return user;
   }
 }
--- src/domains/users/users.service.ts
+++ src/domains/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Users } from './entities/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly usersService: UsersService) {}
+
+    async getUserById(userId: number): Promise<Users> {
+        return await this.usersService.getUserById(userId)
+    }
+}
