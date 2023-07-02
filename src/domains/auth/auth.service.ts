525ffa6b44fab86d4ac23f2d29fbc686519dfa8a
knowave <kwaktaemin98@gmail.com>
2023-07-02 14:25:45 +0900

    유저 유효성 검사

--- src/domains/auth/auth.service.ts
+++ src/domains/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { compare } from 'bcrypt';
+import { AUTH_EXCEPTION } from '../../exception/error-code';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async validateUser(userId: number, plainTextPassword: string): Promise<any> {
+    const user = await this.usersService.getUserById(userId);
+    await this.verifyPassword(plainTextPassword, user.password);
+    const { password, ...result } = user;
+    return result;
+  }
+
+  private async verifyPassword(password: string, hashedPassword: string) {
+    const isPasswordMatch = await compare(password, hashedPassword);
+    if (!isPasswordMatch) {
+      throw new BadRequestException(AUTH_EXCEPTION.AUTH_FAIL_VALIDATE);
+    }
+  }
+}
