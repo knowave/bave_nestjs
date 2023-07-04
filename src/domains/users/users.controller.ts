00e844cba27636fea1716f5fe0931bafcea22420
knowave <kwaktaemin98@gmail.com>
2023-07-04 14:18:36 +0900

    user controller

--- src/domains/users/users.controller.ts
+++ src/domains/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Users } from './entities/users.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  /**
+   * 특정 유저 조회
+   * @param userId 
+   * @returns 
+   */
+  @Get(':userId')
+  async getUserById(@Param('userId') userId: number): Promise<Users> {
+    return await this.usersService.getUserById(userId);
+  }
+}
