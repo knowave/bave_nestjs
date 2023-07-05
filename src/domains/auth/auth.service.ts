84dd85284ccddb813bd3cb323842c871fbd3f0c6
knowave <kwaktaemin98@gmail.com>
2023-07-05 17:12:32 +0900

    auth service logic

--- src/domains/auth/auth.service.ts
+++ src/domains/auth/auth.service.ts
@@ -13,6 +13,12 @@ export class AuthService {
     private configService: ConfigService,
   ) {}
 
+  /**
+   * 비밀번호 유효성 검사한 유저 정보 조회
+   * @param userId
+   * @param plainTextPassword
+   * @returns
+   */
   async validateUser(userId: number, plainTextPassword: string): Promise<any> {
     const user = await this.usersService.getUserById(userId);
     await this.verifyPassword(plainTextPassword, user.password);
@@ -20,10 +26,42 @@ export class AuthService {
     return result;
   }
 
+  /**
+   * 비밀번호 비교
+   * @param password
+   * @param hashedPassword
+   */
   private async verifyPassword(password: string, hashedPassword: string) {
     const isPasswordMatch = await compare(password, hashedPassword);
     if (!isPasswordMatch) {
       throw new BadRequestException(AUTH_EXCEPTION.AUTH_FAIL_VALIDATE);
     }
+  }
+
+  // AccessToken 발급
+  createAcessToken(userId: number) {
+    const payload = { userId };
+
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_ACCESS_TOKEN_SECRET'),
+      expiresIn: `${this.configService.get(
+        'JWT_ACCESS_TOKEN_EXPIRATION_TIME',
+      )}`,
+    });
+  }
+
+  /**
+   * RefreshToken 발급
+   * @param userId
+   * @returns
+   */
+  createRefreshToken(userId: string) {
+    const payload = { userId };
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_REFRESH_TOKEN_SECRET'),
+      expiresIn: `${this.configService.get(
+        'JWT_REFRESH_TOKEN_EXPIRATION_TIME',
+      )}`,
+    });
   }
 }
