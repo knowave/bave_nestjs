be3fcdd615f14ca4711b3e04d798b0141185eac0
knowave <kwaktaemin98@gmail.com>
2023-07-12 15:01:09 +0900

    likes module

--- src/domains/likes/likes.module.ts
+++ src/domains/likes/likes.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { LikesController } from './likes.controller';
+import { LikesRepository } from './likes.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Likes } from './entities/likes.entity';
 
 @Module({
-  providers: [LikesService],
-  controllers: [LikesController]
+  imports: [TypeOrmModule.forFeature([Likes])],
+  providers: [LikesService, LikesRepository],
+  controllers: [LikesController],
 })
 export class LikesModule {}
