985ea10e10de195e6e43a7d48e348026944f48ba
knowave <kwaktaemin98@gmail.com>
2023-07-12 14:58:14 +0900

    like repository 수정

--- src/domains/likes/likes.repository.ts
+++ src/domains/likes/likes.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { Likes } from './entities/likes.entity';
+import { LikeDto } from './dto/like.dto';
 
 @Injectable()
 export class LikesRepository extends Repository<Likes> {
@@ -9,99 +10,32 @@ export class LikesRepository extends Repository<Likes> {
   }
 
   /**
-   * 해수욕장 좋아요 조회
-   * @param userId
-   * @param beachId
+   * 좋아요 조회
+   * @param likeDto
    * @returns
    */
-  async getLikeByBeach(userId: number, beachId: number): Promise<Likes> {
-    return await this.findOne({ where: { userId, beachId } });
+  async getLikeById(likeDto: LikeDto): Promise<Likes> {
+    const { userId, beachId, feedId, replyId }: LikeDto = likeDto;
+
+    return await this.findOne({ where: { userId, beachId, feedId, replyId } });
   }
 
-  /**
-   * 피드 좋아요 조회
-   * @param userId
-   * @param feedId
-   * @returns
-   */
-  async getLikeByFeed(userId: number, feedId: number): Promise<Likes> {
-    return await this.findOne({ where: { userId, feedId } });
-  }
+  async createLike(likeDto: LikeDto): Promise<Likes> {
+    const { userId, beachId, feedId, replyId }: LikeDto = likeDto;
 
-  /**
-   * 댓글 좋아요 조회
-   * @param userId
-   * @param replyId
-   * @returns
-   */
-  async getLikeByReply(userId: number, replyId: number): Promise<Likes> {
-    return await this.findOne({ where: { userId, replyId } });
-  }
-
-  /**
-   * 해수욕장 좋아요
-   * @param userId
-   * @param beachId
-   * @returns
-   */
-  async createLikeByBeach(userId: number, beachId: number): Promise<Likes> {
-    const create = this.create({
+    const like = this.create({
       userId,
       beachId,
-    });
-
-    return await this.save(create);
-  }
-
-  /**
-   * 피드 좋아요 생성
-   * @param userId
-   * @param feedId
-   * @returns
-   */
-  async createLikeByFeed(userId: number, feedId: number): Promise<Likes> {
-    const create = this.create({
-      userId,
       feedId,
-    });
-
-    return await this.save(create);
-  }
-
-  /**
-   * 댓글 좋아요 생성
-   * @param userId
-   * @param replyId
-   * @returns
-   */
-  async createLikeByReply(userId: number, replyId: number): Promise<Likes> {
-    const create = this.create({
-      userId,
       replyId,
     });
 
-    return await this.save(create);
+    return await this.save(like);
   }
 
-  /**
-   * 해수욕장 좋아요 취소
-   * @param userId
-   * @param beachId
-   */
-  async cancelLikeByBeach(userId: number, beachId: number): Promise<void> {
-    await this.delete({ userId, beachId });
-  }
+  async cancelLike(likeDto: LikeDto): Promise<void> {
+    const { userId, beachId, feedId, replyId }: LikeDto = likeDto;
 
-  /**
-   * 피드 좋아요 취소
-   * @param userId
-   * @param feedId
-   */
-  async cancelLikeByFeed(userId: number, feedId: number): Promise<void> {
-    await this.delete({ userId, feedId });
-  }
-
-  async cancelLikeByReply(userId: number, replyId: number): Promise<void> {
-    await this.delete({ userId, replyId });
+    await this.delete({ userId, beachId, feedId, replyId });
   }
 }
