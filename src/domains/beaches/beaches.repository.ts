d9db5e933feb6012d45b7d43f105672dcb3021d9
knowave <kwaktaemin98@gmail.com>
2023-07-15 12:57:52 +0900

    경로 변경

--- src/domains/beaches/beaches.repository.ts
+++ src/domains/beaches/beaches.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Beaches } from './entities/beaches.entity';
 import { DataSource, Repository } from 'typeorm';
-import { BEACH_EXCEPTION } from 'src/exception/error-code';
+import { BEACH_EXCEPTION } from '../../exception/error-code';
 
 @Injectable()
 export class BeachesRepository extends Repository<Beaches> {
